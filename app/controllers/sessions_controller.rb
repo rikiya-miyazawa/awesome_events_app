fa997081eb2cf173a067cc30965a2742fc919f57
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-05-09 21:28:57 +0900

    feat:ログイン用のcreateメソッドを実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,10 @@
 class SessionsController < ApplicationController
+  def create
+    #request.env["omniauth.auth"]というHashに似たOmniAuth::AuthHashクラスのオブジェクトを利用
+    #GitHubから渡されたユーザ情報や、OAuthのアクセストークンなどが格納されている
+    user = User.find_or_create_from_auth_hash!(request.env["omniauth.auth"])
+    #User.find_or_create_from_auth_hash!が返すUserオブジェクトのIDをセッションに格納することでログイン扱いとする
+    session[:user_id] = user.id
+    redirect_to root_path, notice: "ログインしました"
+  end
 end
