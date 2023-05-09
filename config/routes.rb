2ead3f6edd743d2c4bb96e425399a641e7b679b7
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-05-09 21:51:45 +0900

    feat:認証が成功した場合のルーティングを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  #OmniAuthによる認証が成功した場合、デフォルトで/auth/:provider/callbackがコールバック用のURLとして利用される
+  #それをログイン用のアクションと紐付ける
+  get "/auth/:provider/callback" => "sessions#create"
 end
