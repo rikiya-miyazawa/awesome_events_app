bf3dc8b70165e36d4d849de333a84d5504539177
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-05-12 05:20:54 +0900

    feat:ログアウト処理を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def create
     #request.env["omniauth.auth"]というHashに似たOmniAuth::AuthHashクラスのオブジェクトを利用
     #GitHubから渡されたユーザ情報や、OAuthのアクセストークンなどが格納されている
@@ -6,5 +7,11 @@ class SessionsController < ApplicationController
     #User.find_or_create_from_auth_hash!が返すUserオブジェクトのIDをセッションに格納することでログイン扱いとする
     session[:user_id] = user.id
     redirect_to root_path, notice: "ログインしました"
+  end
+
+  def destroy
+    #reset_sessionメソッドでセッションに入っていた値が全て削除される
+    reset_session
+    redirect_to root_path, notice: "ログアウトしました"
   end
 end
