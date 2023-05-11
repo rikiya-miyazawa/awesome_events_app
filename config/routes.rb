62208cd8123d7081ac402e5ebed27d717ce792a1
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-05-12 06:02:43 +0900

    feat:ログアウト処理のルーティングを実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   #OmniAuthによる認証が成功した場合、デフォルトで/auth/:provider/callbackがコールバック用のURLとして利用される
   #それをログイン用のアクションと紐付ける
   get "/auth/:provider/callback" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
 end
