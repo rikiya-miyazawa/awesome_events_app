45aae4e130eb2d5d8693f6de725fec3d865fa7ad
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-05-12 05:57:17 +0900

    feat:ログイン状態を確認するlogged_inメソッドを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  #今後コントローラとビューの両方から利用する機会があるのでヘルパーメソッドでメソッド名を宣言しておく
+  helper_method :logged_in?
+
+  private
+
+  def logged_in?
+    #not演算子(!)をふたつ重ねることで
+    #session[:user_id]がfalseまたはnilの時はfalseを、それ以外の値の時はtrueへ変換する
+    !!session[:user_id]
+  end
 end
