c9b5ff44800bd77f0ace91f25e1da9b07c232a2a
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-05-12 18:49:05 +0900

    feat:userモデルにcreate_eventsメソッドを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  #関連元のユーザが作成したイベントという意味で「create_events」という命名
+  #モデル名以外の名前を関連名に使用したのでclass_nameオプションでクラス名を指定
+  #外部キーもデフォルトの「user_id」ではないのでforeign_keyオプションで外部キー(owner_id)を指定
+  has_many :created_events, class_name: "Event", foreign_key: "owner_id"
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
