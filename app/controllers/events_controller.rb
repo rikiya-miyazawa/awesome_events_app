e7d063b578d4a53b9902726186bad72935040dcf
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-05-12 18:28:35 +0900

    feat:イベント作成機能のコントローラを実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,23 @@
 class EventsController < ApplicationController
+
+  def new
+    #ログインユーザが作成したイベントを@event変数に代入する
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @events.save
+      redirect_to @event, notice: "作成しました"
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :name, :place, :content, :start_at, :end_at
+    )
+  end
 end
