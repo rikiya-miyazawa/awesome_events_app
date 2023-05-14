e9ec041b6bf74a1f11e2422403da1056856ae39a
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-05-15 08:21:34 +0900

    feat:未開催のイベントを開催時間の昇順でイベント一覧画面に表示する機能を実装

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate
   def index
+    #未開催のイベントを開催時間が近い順(昇順)で取得する
+    @events = Event.where("start_at > ?", Time.zone.now).order(:start_at)
   end
 end
