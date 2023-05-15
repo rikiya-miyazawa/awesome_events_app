bd9d7e14dbb4558143365d54c4b13edfd985a746
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-05-15 21:29:55 +0900

    ticketモデルを実装

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,5 @@
 class Ticket < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true  #optional: true - 関連先がなくてもバリデーションエラーにならないように
   belongs_to :event
+  validates :comment, length: { maximum: 30 }, allow_blank: true  #allow_blank: true 空文字やnilを許可
 end
