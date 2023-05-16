79307fdf9f09510af8339ccd46331f20b4200dbf
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-05-16 16:39:18 +0900

    feat:eventの子の関係としてticketのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :events
   root 'welcome#index'
   #OmniAuthによる認証が成功した場合、デフォルトで/auth/:provider/callbackがコールバック用のURLとして利用される
   #それをログイン用のアクションと紐付ける
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+  resources :events do
+    resources :tickets
+  end
 end
