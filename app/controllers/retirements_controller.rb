bf5914f56790e203ff25cf382f9981f87d6704b2
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-05-17 10:57:02 +0900

    feat:ユーザ削除に対応する関連を実装

--- app/controllers/retirements_controller.rb
+++ app/controllers/retirements_controller.rb
@@ -3,6 +3,7 @@ class RetirementsController < ApplicationController
   end
 
   def create
+    #ユーザレコードの削除が成功したら、セッションを削除してログアウト状態にしている
     if current_user.destroy
       reset_session
       redirect_to root_path, notice: "退会完了しました"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  before_destroy :check_all_events_finished  #Userモデルの削除時に実行されるコールバックを追加
   #関連元のユーザが作成したイベントという意味で「create_events」という命名
   #モデル名以外の名前を関連名に使用したのでclass_nameオプションでクラス名を指定
   #外部キーもデフォルトの「user_id」ではないのでforeign_keyオプションで外部キー(owner_id)を指定
-  has_many :created_events, class_name: "Event", foreign_key: "owner_id"
-  has_many :tickets
+  has_many :created_events, class_name: "Event", foreign_key: "owner_id", dependent: :nullify
+  has_many :tickets, dependent: :nullify  #dependent: :nullifyオプションで削除した時に関連するレコードの外部キーをnullにできる
+  has_many :participating_events, through: :tickets, source: :event
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
@@ -16,5 +18,25 @@ class User < ApplicationRecord
       user.name = nickname
       user.image_url = image_url
     end
+  end
+
+  private
+
+  def check_all_events_finished
+    now = Time.zone.now
+    #現在の時間より終了時間が後のイベント、つまり「未終了」のイベントがあるか調べる
+    #もしも未終了のイベントが存在する場合はerrorsオブジェクトにエラーメッセージを追加する
+    if created_events.where(":now < end_at", now: now).exists?
+      #erroes[:base]はActiveRecordモデルのエラーオブジェクトに対してエラーメッセージを追加するための特殊なキー
+      #errors[:base] を使用すると、モデル全体に関連するエラーメッセージを追加することができる
+      errors[:base] << "公開中の未終了イベントが存在します。"
+    end
+
+    #ユーザが参加しているイベントを調べる
+    if participating_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "未終了の参加イベントが存在します。"
+    end
+
+    throw(:abort) unless errors.empty?
   end
 end
